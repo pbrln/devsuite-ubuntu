d2ea60c4625ea76fe9a115901ca9000c92d8144f
Pascal Berlin <ecb2@ecb2.pw>
2017-05-23 21:05:42 +0200

    added nodejs & npm

--- dev-suite.sh
+++ dev-suite.sh
@@ -4,11 +4,20 @@
 # License: MIT
 
 function installAll {
-    echo "Installing all tools"
+    installNode
 }
 
 function installNode {
     #Add Node to source repos
+    printf "Adding node repository"
+    curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - > /dev/null
+    echo " [DONE]"
+    printf "Installing NodeJS"
+    apt-get install -y nodejs > /dev/null
+    echo " [DONE]"
+    printf "Installing NPM"
+    apt-get install -y npm > /dev/null
+    echo " [DONE]"
 }
 
 function installIndividual {
@@ -21,6 +30,7 @@ echo "- NodeJS, NPM, NVM"
 echo "- Visual Studio Code"
 echo "- python"
 echo "- make, cmake, g++"
+echo "- build-essential"
 while true; do
     read -p "Do you wish to install the whole package? Otherwise you'll be asked for each package. [Y/n] " installAll
     case $installAll in
