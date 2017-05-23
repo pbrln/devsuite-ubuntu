1fa891b34a4b2605bbdf474870f718f7e768a9bf
Pascal Berlin <ecb2@ecb2.pw>
2017-05-23 20:50:18 +0200

    changes

--- dev-suite.sh
+++ dev-suite.sh
@@ -17,8 +17,10 @@ echo "- NodeJS, NPM, NVM"
 echo "- Visual Studio Code"
 echo "- python"
 echo "- make, cmake, g++"
-read -p "Do you wish to install the whole package? Otherwise you'll be asked for each package. Y/n: " installAll
-case $installAll in
-    [Yy]* ) installAll
-    [Nn]* ) installIndividual
-esac
+while true; do
+    read -p "Do you wish to install the whole package? Otherwise you'll be asked for each package. [Y/n] " installAll
+    case $installAll in
+        [Yy]* ) installAll; break;;
+        [Nn]* ) installIndividual; break;;
+    esac
+done
