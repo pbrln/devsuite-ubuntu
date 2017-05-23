c0619eecbf2294d1a85115a7eb4edf80d8750aa0
Pascal Berlin <ecb2@ecb2.pw>
2017-05-23 21:17:33 +0200

    added all and everything and really hoping it works

--- dev-suite.sh
+++ dev-suite.sh
@@ -5,6 +5,10 @@
 
 function installAll {
     installNode
+    installVSCode
+    installPython
+    installMakeTools
+    installBuildEssential
 }
 
 function installNode {
@@ -15,13 +19,79 @@ function installNode {
     printf "Installing NodeJS"
     apt-get install -y nodejs > /dev/null
     echo " [DONE]"
-    printf "Installing NPM"
-    apt-get install -y npm > /dev/null
+    #printf "Installing NPM"
+    #apt-get install -y npm > /dev/null
+    #echo " [DONE]"
+}
+
+function installVSCode {
+    printf "Adding Visual Studio Code repository"
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg > /dev/null
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' > /dev/null
+    echo " [DONE]"
+    printf "Installing Visual Studio Code"
+    apt-get update > /dev/null
+    apt-get install code > /dev/null
+    echo " [DONE]"
+}
+
+function installPython {
+    printf "Installing python"
+    apt-get install python3 > /dev/null
+    echo " [DONE]"
+}
+
+function installMakeTools {
+    printf "Installing make, cmake, g++"
+    apt-get install make > /dev/null
+    apt-get install cmake > /dev/null
+    apt-get install g++ > /dev/null
+    echo " [DONE]"
+}
+
+function installBuildEssential {
+    printf "Installing build-essential"
+    apt-get install build-essential > /dev/null
     echo " [DONE]"
 }
 
 function installIndividual {
-    echo "Prompt for each"
+    while true; do
+        read -p "Do you wish to install NodeJS? [Y/n] " installYN
+        case $installAll in
+            [Yy]* ) installNode; break;;
+            [Nn]* ) break;;
+        esac
+    done
+    while true; do
+        read -p "Do you wish to install Visual Studio Code? [Y/n] " installYN
+        case $installAll in
+            [Yy]* ) installVSCode; break;;
+            [Nn]* ) break;;
+        esac
+    done
+    while true; do
+        read -p "Do you wish to install python? [Y/n] " installYN
+        case $installAll in
+            [Yy]* ) installPython; break;;
+            [Nn]* ) break;;
+        esac
+    done
+    while true; do
+        read -p "Do you wish to install make, cmake, g++? [Y/n] " installYN
+        case $installAll in
+            [Yy]* ) installMakeTools; break;;
+            [Nn]* ) break;;
+        esac
+    done
+    while true; do
+        read -p "Do you wish to install build-essential? [Y/n] " installYN
+        case $installAll in
+            [Yy]* ) installBuildEssential; break;;
+            [Nn]* ) break;;
+        esac
+    done
 }
 printf "\033c"
 echo "Ubuntu Development Suite Installer"
