e449a69f403825072450da834386fa8e3707aef8
Pascal Berlin <ecb2@ecb2.pw>
2017-05-27 16:57:10 +0200

    fixes again

--- dev-suite.sh
+++ dev-suite.sh
@@ -78,7 +78,14 @@ function installZshWithZim {
 	chsh -s /usr/bin/zsh > /dev/null
     echo " [DONE]"
 	printf "Installing zim"
+	zsh
     git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
+	setopt EXTENDED_GLOB
+	for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
+	  user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+	  touch ${user_file}
+	  ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
+	done
 	source ${ZDOTDIR:-${HOME}}/.zlogin
 	echo " [DONE]"
 }
